2e4597856373d340ea70eec46409408aaf547921
Ian Cenry <iancenry@gmail.com>
2024-01-07 19:36:09 +0300

    chore: add icon package

--- components/Sidebar/Left.vue
+++ components/Sidebar/Left.vue
@@ -1,11 +1,20 @@
 <template>
-  <div
-    class="p-2 my-2 hover:scale-105 hover:bg-blue-50 hover:rounded-full w-min dark:hover:bg-white/20"
-  >
-    <NuxtLink to="/">
-      <div class="h-8 w-8">
-        <LogoTwitter />
-      </div>
-    </NuxtLink>
+  <div class="flex flex-col h-screen">
+    <div
+      class="p-2 my-2 hover:scale-105 rounded-full w-min hover:bg-blue-50 dark:hover:bg-white/20"
+      :class="defaultTransition"
+    >
+      <NuxtLink to="/">
+        <div class="h-8 w-8">
+          <LogoTwitter />
+        </div>
+      </NuxtLink>
+    </div>
+    <HomeIcon />
   </div>
 </template>
+
+<script setup>
+import { HomeIcon } from '@heroicons/vue/solid';
+const { defaultTransition } = useTailwindConfig();
+</script>
