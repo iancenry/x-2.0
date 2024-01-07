4ffc9cb63a1447fd2d67ccf66fa405f1c262531f
Ian Cenry <iancenry@gmail.com>
2024-01-07 19:03:18 +0300

    tailwinf config & start left sidebar

---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="p-2 my-2 hover:scale-105 hover:bg-blue-50 hover:rounded-full w-min dark:hover:bg-white/20"
+  >
+    <NuxtLink to="/">
+      <div class="h-8 w-8">
+        <LogoTwitter />
+      </div>
+    </NuxtLink>
+  </div>
+</template>
